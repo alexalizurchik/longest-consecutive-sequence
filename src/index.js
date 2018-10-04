128af7944d698bc8d77c637dfe80febdf7d08b12
Alexandrina <lizurchik.a@gmail.com>
2018-10-04 23:57:35 +0300

    Add implementation of longestConsecutiveLength function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports = function longestConsecutiveLength(arr) {
+ if (arr.length === 0) {
+    return 0;
+  }
+  
+  let hash = {};
+  let longestSequence = 1;
+  let currentSequence = 1;
+  
+  for (let i = 0; i < arr.length; i++) {
+    hash[arr[i]] = true;
+  }
+  
+  for (let key in hash) {
+    if (hash[+(key) + 1]) {
+      currentSequence++;
+    } else {
+      currentSequence = 1;
+    }
+      
+    if (currentSequence > longestSequence) {
+      longestSequence = currentSequence;
+    }
+  }
+  
+  return longestSequence;
 }
